23d42ebd290bcd83d9f1d5ae0a298737e22b0609
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-04-22 21:17:34 +0100

    Create second level

--- src/scenes/level2.lua
+++ src/scenes/level2.lua
@@ -3,34 +3,31 @@ local Player = require('src.entities.player')
 local Asteroid = require('src.entities.asteroid')
 local Star = require('src.entities.star')
 
-local level1 = {}
+local level2 = {}
 
-function level1.load()
+function level2.load()
     entityManager = EntityManager.create()
     for i = 1, 100 do
         entityManager:addEntity(Star.create(love.math.random(0, love.graphics.getHeight())))
     end
-    for i = 1, 2 do
+    for i = 1, 5 do
         entityManager:addEntity(Asteroid.create(entityManager, love.math.random(0, love.graphics.getWidth()), love.math.random(0, love.graphics.getHeight())))
     end
     entityManager:addEntity(Player.create(entityManager, love.graphics.getWidth() / 2, love.graphics.getHeight() / 2))
 end
 
-function level1.update(dt)
+function level2.update(dt)
     entityManager:update(dt)
 end
 
-function level1.draw()
+function level2.draw()
     entityManager:draw()
 end
 
-function level1.keypressed(key)
+function level2.keypressed(key)
     if key == 'escape' then
         Scene.Load("menu")
     end
-    if key == 'return' then
-        Scene.Load('level2')
-    end
 end
 
-return level1
+return level2
